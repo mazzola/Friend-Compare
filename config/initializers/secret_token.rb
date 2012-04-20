f096f29cd312e1d8a3c4bba67d4bbad15393f41c
Michael Mazzola <mazzola70@gmail.com>
2012-04-19 21:48:31 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FriendCompare::Application.config.secret_token = 'ff029380799f6d257c4db02fd2252dcb59612e37b57f23ca8d863fe27b663792f5d8dcdb89098010b977fb821919266471dfdb90fe715a73f04c024ff2b4e56e'
