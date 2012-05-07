659520db3ff4805b82d86ab19e3ec13e1bf1ec7b
Michael Mazzola <mazzola70@gmail.com>
2012-05-07 09:51:24 -0400

    Chanign fb params to production

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '452750491408552', '7f5204080d0a1707b7c4ea9880c27603',
+  provider :facebook, '344245222304466', '424d24d1bb5439f7cd491087a5a55152',
            {:scope => 'email,offline_access,read_stream'}
 end
