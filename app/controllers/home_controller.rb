3f21d0ca103af3ed33ccaf3f858b6ae1ce0ca732
Michael Mazzola <mazzola70@gmail.com>
2012-05-07 09:54:17 -0400

    bullshit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -19,6 +19,6 @@ class HomeController < ApplicationController
 	end
 	def create
 		auth_hash = request.env['omniauth.auth']
-  	render :text => auth_hash.inspect
+		render :text => auth_hash.inspect
 	end
 end
